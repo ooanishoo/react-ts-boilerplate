b86b55f3fd013d072ee05094b575f2c90ac69cbe
ooanishoo <anish.maharjan12341@gmail.com>
2021-04-07 10:41:11 +1000

    Add react-router-dom
    - Install react-router-dom and @types/react-router-dom packages
    - Move @types packages from dependencies to devDependencies

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Router>
+            <App />
+        </Router>
     </React.StrictMode>,
     document.getElementById('root')
 );
