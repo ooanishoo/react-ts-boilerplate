b9e4bad46677d4e8f39cc08f5d092d5c3d593b1f
ooanishoo <anish.maharjan12341@gmail.com>
2021-04-09 15:28:22 +1000

    axios mock working properly

--- src/api/index.spec.ts
+++ src/api/index.spec.ts
@@ -1,27 +1,67 @@
 import { cleanup } from '@testing-library/react';
 import { getTodo, getTodos } from '.';
 import { Todo } from '../types';
+import axios, { AxiosResponse } from 'axios';
 
 afterEach(cleanup);
 
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
 describe('getTodos()', () => {
     test('should return todo list', async () => {
+        const todos: Todo[] = [
+            {
+                userId: 1,
+                id: 1,
+                title: 'todo-test-1',
+                completed: false,
+            },
+            {
+                userId: 2,
+                id: 2,
+                title: 'todo-test-2',
+                completed: true,
+            },
+        ];
+        const mockedResponse: AxiosResponse = {
+            data: todos,
+            status: 200,
+            statusText: 'OK',
+            headers: {},
+            config: {},
+        };
+        mockedAxios.get.mockResolvedValue(mockedResponse);
+        expect(axios.get).not.toHaveBeenCalled();
         const data = await getTodos();
-        expect(data).toBeTruthy;
+        expect(axios.get).toHaveBeenCalled();
+        expect(data).toEqual(todos);
     });
 });
 
 describe('getTodo()', () => {
     test('should return a single todo', async () => {
-        jest.mock('axios');
         const todo: Todo = {
             userId: 1,
             id: 1,
             title: 'delectus aut autem',
             completed: false,
         };
-        // axios.get.mockResolvedValueOnce(todo);
-        const data: Todo = await getTodo(1);
+        const mockedResponse: AxiosResponse = {
+            data: todo,
+            status: 200,
+            statusText: 'OK',
+            headers: {},
+            config: {},
+        };
+        mockedAxios.get.mockResolvedValue(mockedResponse);
+
+        expect(axios.get).not.toHaveBeenCalled();
+        const data: Todo = await getTodo(1232323);
+        expect(axios.get).toHaveBeenCalled();
         expect(data).toEqual(todo);
     });
 });
+
+//jest.mock('axios');
+//jest.mock('axios', () => jest.fn());
