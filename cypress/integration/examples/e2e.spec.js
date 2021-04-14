b819e6026ccd7e389fa9fae710c714b5b5176d36
ooanishoo <anish.maharjan12341@gmail.com>
2021-04-14 12:19:34 +1000

    Integrate Cypress
    1. Install following dependencies:
    - @cypress/react: Primary way to mount and interact with components (similar to mount in Enzyme or render in Testing Library).
    - @testing-library/cypress : allows you to use all the useful DOM Testing Library methods in your tests.
    
    2. Add npm run e2e script to open cypress
    
    3. Add e2e.yml to run cypress on github actions

---
+++ cypress/integration/examples/e2e.spec.js
@@ -0,0 +1,27 @@
+/* To get really good tooling and autocomplete */
+/// <reference types="Cypress" />
+
+beforeEach(() => {
+  cy.visit("http://localhost:3000");
+});
+
+it("has a title", () => {
+  cy.contains("My Project");
+});
+
+it("navigates between pages", () => {
+  //cy.pause();
+  cy.clickLink("Todos");
+  cy.url().should("include", "/todos");
+  cy.clickLink("Home");
+  cy.url().should("not.include", "/todos");
+});
+
+it("should render todos", () => {
+  cy.goToTodos(); // call custom command
+  cy.get('[data-testid="todo-list"]').as("todos"); // using alias
+  cy.get("@todos")
+    .children()
+    .should("be.visible")
+    .and("have.length.at.least", 1);
+});
